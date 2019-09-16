05978d393c84f6078c727e9d497a2e6dd161a640
Luca Terracciano <terraccianoluca@gmail.com>
2019-09-16 16:26:08 +0200

    Added commnets to the code

--- app_mp_mpi_4.c
+++ app_mp_mpi_4.c
@@ -57,10 +57,16 @@ int main(int argc, char *argv[])
     int n_threads = argv[8];
 
     //Creating data structures
+
+    //Partial array of points
     double *point_x = malloc(sizeof(double) * n_points / world_size);
     double *point_y = malloc(sizeof(double) * n_points / world_size);
+
+    //Total array of points
     double *p_x = malloc(sizeof(double) * n_points);
     double *p_y = malloc(sizeof(double) * n_points);
+
+    //Total array of centroids
     double *centroid_x = malloc(sizeof(double) * n_centroids);
     double *centroid_y = malloc(sizeof(double) * n_centroids);
 
@@ -69,20 +75,6 @@ int main(int argc, char *argv[])
     {
         read_points(argv[1], n_points, p_x, p_y);
         read_points(argv[3], n_centroids, centroid_x, centroid_y);
-        /*
-        #pragma omp parallel for
-        for (int i = 0; i < n_points; i++)
-        {
-            p_x[i] = points[i].x;
-            p_y[i] = points[i].y;
-        }
-        #pragma omp parallel for
-        for (int i = 0; i < n_centroids; i++)
-        {
-            centroid_x[i] = centroids[i].x;
-            centroid_y[i] = centroids[i].y;
-        }
-        */
     }
 
     //Scattering points
@@ -107,16 +99,25 @@ int main(int argc, char *argv[])
         MPI_COMM_WORLD);
 
     //Support data structures
+
+    //Total array of the new centroids
     double *new_centroid_x = malloc(n_centroids * sizeof(double));
     double *new_centroid_y = malloc(n_centroids * sizeof(double));
-    int *assign_centroid = malloc(n_points* sizeof(int));
+    //Counts the number of points associated to the new centroid
     int *new_centroids_n_points = malloc(n_centroids * sizeof(int));
-    int *global_new_centroids_n_points = malloc(n_centroids * sizeof(int));
+    //For every points saves which centroid is the nearest
+    int *assign_centroid = malloc(n_points* sizeof(int));
+    //Partial array of new centroids
     double *local_new_centroid_x = malloc(n_centroids/world_size * sizeof(double));
     double *local_new_centroid_y = malloc(n_centroids/world_size * sizeof(double));
+    //Counts the number of points associated to the new local centroid
     int *local_new_centroids_n_points = malloc(n_centroids/world_size * sizeof(int));
+    //
     double *global_new_centroid_x = malloc(n_centroids * sizeof(double));
     double *global_new_centroid_y = malloc(n_centroids * sizeof(double));
+    //
+    int *global_new_centroids_n_points = malloc(n_centroids * sizeof(int));
+    //Indicates for each node how many centroid it will receive
     int *recvcounts = malloc(world_size * sizeof(int));
     double curr_error;
 
@@ -150,6 +151,8 @@ int main(int argc, char *argv[])
 
         //Resetting support data structures
         curr_error = 0;
+
+        //Initialize the Total new centroids array to 0
         #pragma omp parallel for
         for (int i = 0; i < n_centroids; i++)
         {
@@ -158,6 +161,7 @@ int main(int argc, char *argv[])
             new_centroids_n_points[i] = 0;
         }
 
+        //Initialize the Local new centroids array to 0
         #pragma omp parallel for
         for (int i = 0; i < n_centroids / world_size; i++)
         {
@@ -189,14 +193,21 @@ int main(int argc, char *argv[])
             //It will be averaged.
             //#pragma omp critical
             //{
+                //Sum the coordinates of the points asssociated to the closest_centroid 
+                //(the new centroid will be calculated later by averaging this two arrays with the number of points associated to the closest_centroid)
                 new_centroid_x[closest_centroid] += point_x[i];
                 new_centroid_y[closest_centroid] += point_y[i];
+                //Count the points associated to the closest centroid
                 new_centroids_n_points[closest_centroid]++;
             //  }
             
         }
         //Reduce the partial computation.
         //Scatter all new centroids across all nodes.
+        //Reduce all the new_centroid arrays into one and then scatter again across all nodes
+
+        //At the first iteration each node has a partition of the points and the full set of the centroids.
+        //In the other iterations each node will have a partition of the points and a partiton of the centroids.
         MPI_Reduce_scatter(
             new_centroid_x,
             local_new_centroid_x,
@@ -220,21 +231,33 @@ int main(int argc, char *argv[])
             MPI_INT,
             MPI_SUM,
             MPI_COMM_WORLD);
+        
+        //offset indicates which portion of the new_centroid arrays is assigned to the current node.
+        //i.e the centroids are 20
+        //Node 1: new_centroids_*[0:9]
+        //Node 2: new_centroids_*[10:19]
 
-            int offset = world_rank * (n_centroids / world_size);
-            #pragma omp parallel for reduction(+: curr_error)
-            for (int i = 0; i < n_centroids/world_size; i++)
+        int offset = world_rank * (n_centroids / world_size);
+        
+        #pragma omp parallel for reduction(+: curr_error)
+        //Iterate over the partition of the new_centroids
+        for (int i = 0; i < n_centroids/world_size; i++)
+        {
+            if (local_new_centroids_n_points[i] != 0)
             {
-                if (local_new_centroids_n_points[i] != 0)
-                {
-                    local_new_centroid_x[i] = local_new_centroid_x[i] / local_new_centroids_n_points[i];
-                    local_new_centroid_y[i] = local_new_centroid_y[i] / local_new_centroids_n_points[i];
-                    curr_error += calc_distance(centroid_x[offset + i], centroid_y[offset + i], local_new_centroid_x[i], local_new_centroid_y[i]);
-                } else {
-                    local_new_centroid_x[i] = centroid_x[offset+i];
-                    local_new_centroid_y[i] = centroid_y[offset+i];
-                }
+                //Averaging the Local new centroids
+                local_new_centroid_x[i] = local_new_centroid_x[i] / local_new_centroids_n_points[i];
+                local_new_centroid_y[i] = local_new_centroid_y[i] / local_new_centroids_n_points[i];
+                //Calculate the local distance from the old one
+                //centroid_* : Total array of centroids
+                //local_new_centroids_* : Local array of the new centroids
+                curr_error += calc_distance(centroid_x[offset + i], centroid_y[offset + i], local_new_centroid_x[i], local_new_centroid_y[i]);
+            } else {
+                //If no points are associated to the centroid, it will write the old value of the centroid
+                local_new_centroid_x[i] = centroid_x[offset+i];
+                local_new_centroid_y[i] = centroid_y[offset+i];
             }
+        }
 
         //Sum all curr_error
         //Broadcast the result to all node.
@@ -246,7 +269,7 @@ int main(int argc, char *argv[])
             MPI_SUM,
             MPI_COMM_WORLD
         );
-        //Gather all the centroids to all node.
+        //Gather all the centroids and send the total array to all nodes
         MPI_Allgather(
             local_new_centroid_x,
             n_centroids / world_size,
